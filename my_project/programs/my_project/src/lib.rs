6fb8c354f3df38aed67152cdc1a464c6be506daa
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2026-01-02 12:07:00 +0600

    Commit message for 2 January Tarikh (repeat yet again)

--- my_project/programs/my_project/src/lib.rs
+++ my_project/programs/my_project/src/lib.rs
@@ -456,3 +456,19 @@
 // pub supply: u64,
 
 // pub mint: Account<'info , Mint>
+
+// #[derive(Accounts)]
+// pub struct CreateMint<'info> {
+//     #[account(mut)]
+//     pub signer: Signer<'info>,
+//     #[account(
+//         init,
+//         payer = signer,
+//         mint::decimals = 6,
+//         mint::authority = signer.key(),
+//         mint::freeze_authority = signer.key()
+//     )]
+//     pub mint: InterfaceAccount<'info, Mint>,
+//     pub token_program: Interface<'info, TokenInterface>,
+//     pub system_program: Program<'info, System>,
+// }
