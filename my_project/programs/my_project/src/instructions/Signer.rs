a6e664d9ba38452cbdd1207fd55aeb48f09b3334
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2026-01-03 12:07:00 +0600

    Commit message for 3 January Tarikh (repeat once again again)

--- my_project/programs/my_project/src/instructions/Signer.rs
+++ my_project/programs/my_project/src/instructions/Signer.rs
@@ -1,13 +1,13 @@
-#[derive(Accounts)]
-pub struct UpdataVault<'info>{
-    #[account(
-        mut, 
-        seeds = [b"vault" , authority.key().as_ref()],
-        bump
-    )]
+// #[derive(Accounts)]
+// pub struct UpdataVault<'info>{
+//     #[account(
+//         mut,
+//         seeds = [b"vault" , authority.key().as_ref()],
+//         bump
+//     )]
 
-    pub valut: Account<'info , vault>,
+//     pub valut: Account<'info , vault>,
 
-    #[account(signer)]
-    pub authority: AccountInfo<'info>,
-}
+//     #[account(signer)]
+//     pub authority: AccountInfo<'info>,
+// }
