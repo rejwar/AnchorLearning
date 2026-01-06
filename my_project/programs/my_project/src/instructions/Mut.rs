d60f6a724b39876bd3653735b6fa8360a58c768e
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2026-01-06 12:07:00 +0600

    Commit message for 6 January Tarikh

--- my_project/programs/my_project/src/instructions/Mut.rs
+++ my_project/programs/my_project/src/instructions/Mut.rs
@@ -20,3 +20,19 @@
 //     #[account(mut)]
 //     pub counter: Account<'info, Counter>,
 // }
+// #[derive(Accoutns)]
+// pub struct Update<'info> {
+//     #[account(mut)]
+//     pub counter: Account<'info, Counter>,
+// }
+
+// #[derive(Accounts)]
+// pub struct Update<'info> {
+//     #[account(mut)]
+//     pub counter: Account<'info, Counter>,
+// }
+
+// pub struct Example<'info> {
+//     pub payer: Signer<'info>,
+//     pub authority: Signer<'info>,
+// }
