0c2e34a1839c521b638239083980ee6495852888
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2026-01-18 10:34:25 +0600

    Error code

--- my_project/programs/my_project/src/lib.rs
+++ my_project/programs/my_project/src/lib.rs
@@ -1,49 +1,10 @@
-use anchor_lang::prelude::*;
+//
 
-declare_id!("alkgfjsfhdgfkjahsfd854q23957243");
+#[error_code]
 
-#[program]
-pub mod example {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let counter = &ctx.accounts.counter;
-        msg!("Counter account created ! Current count {}", counter.count);
-        Ok(())
-    }
-
-    pub fn increment(ctx: Context<Initialize>) -> Result<()> {
-        let counter = &mut ctx.accounts.counter;
-        msg!("Previous counter is {}", counter.count);
-
-        counter.count += 1;
-        msg!("Counter incremented current count is {}", counter.count);
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-
-    #[account (
-        init,
-        payer = payer,
-        space = 8 + 8
-    )]
-    pub counter: Account<'info, Counter>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Account)]
-
-pub struct Increment<'info> {
-    #[account(mut)]
-    pub counter: Account<'info, Counter>,
-}
-
-#[account]
-pub struct Counter {
-    pub count: u64,
+pub enum MyError {
+    #[msg("Your age is under 18")]
+    underage,
+    #[msg("There  is no balance in your account ")]
+    insufficientFunds,
 }
