caa7c446d053fb3acb600c48e61696899e6ab924
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2026-01-28 00:42:51 +0600

    Hello push

---
+++ my_project/programs/my_project/src/instructions/init.rs
@@ -0,0 +1,12 @@
+// #[account(
+//     init,
+//     payer = user,
+//     space = 8 +8
+// )]
+
+// pub my_account: Account<'info , MyAccount
+
+// #[account]
+// pub struct Data {
+//     pub value: u64,
+// }
