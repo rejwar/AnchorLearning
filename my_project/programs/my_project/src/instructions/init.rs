ed957a5267d8625fd510880fb0615f8cea6a9a95
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2026-01-28 00:44:31 +0600

    Hello

--- my_project/programs/my_project/src/instructions/init.rs
+++ my_project/programs/my_project/src/instructions/init.rs
@@ -10,3 +10,10 @@
 // pub struct Data {
 //     pub value: u64,
 // }
+
+// #[account]
+// pub struct User {
+//     pub authority: Pubkey, //32
+//     pub balance: u64,      // 8
+//     pub active: bool,      //1
+// }
